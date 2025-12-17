a0b95b9cf10ae04f025286f319eab3172ed5400d
bikalpa1290 <adhikaribikalpa1@gmail.com>
2025-12-17 16:57:12 +0545

    Completed

--- project2-NotesApp/src/App.jsx
+++ project2-NotesApp/src/App.jsx
@@ -1,43 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from  './App.module.css';
+
 const App = () => {
+  const [title, setTitle] = useState("");
+  const changeTitle=(e)=>{
+  setTitle(e.target.value);
+}
+  
+  const [description, setDescription] = useState("");
+  function changeDescp(e){
+    setDescription(e.target.value);
+}
+const deleteNote=(index)=>{
+  const newTask=[...task]
+  newTask.splice(index,1);
+  setTask(newTask);
+}
+  const [task, setTask] = useState([])
   return (
     <div className={styles.mainDiv}>
       <div className={styles.formDiv}>
-       <form>
-           <input type="text" placeholder='Title' />
-           <textarea placeholder='Description'></textarea>
+
+       <form onSubmit={(e)=>{
+        e.preventDefault();
+        if(!title.trim() || !description.trim())return;//guard clause
+          // const prev=[...task];
+          setTask(prev=>[...prev,{title,description}])
+          setTitle("");
+          setDescription("");
+       }}>
+           <input type="text" placeholder='Title'value={title} onChange={changeTitle}/>
+           <textarea placeholder='Description' value={description} onChange={changeDescp}></textarea>
            <button>Add Note</button>
         </form>
       </div>
       <div className={styles.contentDiv}>
-        <div className={styles.content}>
-          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
-          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
+        
+          {task.map(function(e,index){
+            return(   
+            <div className={styles.content} key={index}>      
+              <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>{e.title}</div>
+              <div className={styles.descp}>{e.description}</div>
+              <span onClick={()=>{deleteNote(index)}}>Delete</span>
+            </div>
+            )               
+          })}
         </div>
-        <div className={styles.content}>
-          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
-          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
-          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
-          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
-          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
-        </div> 
-        <div className={styles.content}>
-          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
-          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
-        </div>                               
-      </div>
+      
     </div>
   )
 }
 
 export default App;
+
