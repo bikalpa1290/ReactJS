bd9f75f61a12beff83eeab24b4fb57ab667e0a0b
bikalpa1290 <adhikaribikalpa1@gmail.com>
2025-12-17 11:27:45 +0545

    improved Css

--- project2-NotesApp/src/App.jsx
+++ project2-NotesApp/src/App.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
-import styles from './App.module.css';
-import Left from './assets/Components/Left/Left';
-import Right from './assets/Components/Left/Right';
-
+import styles from  './App.module.css';
 const App = () => {
   return (
     <div className={styles.mainDiv}>
-      <Left />  
-      <Right />
+      <div className={styles.formDiv}>
+       <form>
+           <input type="text" placeholder='Title' />
+           <textarea placeholder='Description'></textarea>
+           <button>Add Note</button>
+        </form>
+      </div>
+      <div className={styles.contentDiv}>
+        <div className={styles.content}>
+          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
+          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
+          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
+          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
+          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
+          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
+        </div> 
+        <div className={styles.content}>
+          <div className={styles.title} style={{fontSize:"15px",fontWeight:"bold"}}>Hello</div>
+          <div className={styles.descp}>my name is Bikalpa Adhikari i am a very goood guyyyy</div>
+        </div>                               
+      </div>
     </div>
   )
 }
