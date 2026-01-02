82fec6dd6a33d82ca08be79d93294e7341872f77
bikalpa1290 <adhikaribikalpa1@gmail.com>
2026-01-02 15:51:54 +0545

    add some photos

--- Project5-weatherApp/src/App.jsx
+++ Project5-weatherApp/src/App.jsx
@@ -1,35 +1,44 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-
+import styles from "./app.module.css"
 const App = () => {
-  const [response, setResponse] = useState(null);
-
+  const [city, setCity] = useState("");
   const getResponse = async () => {
     try {
       const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=london&appid=72f7f2f8ac1fce8742e904c653c14bc4`
+        `https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid=72f7f2f8ac1fce8742e904c653c14bc4`
       );
-      setResponse(res.data); // sets the API response
       console.log(res.data);
+      console.log("city is update");
     } catch (err) {
       console.error(err.response?.data || err.message);
     }
   };
+const submitHandler=(e)=>{
+  e.preventDefault();
+  console.log("weather is updated",city);
+  setCity("");
+}
 
-  const changeCity = () => {
-    if (!response) return; 
-    setResponse(prev => ({ ...prev, name: "Nepal" })); 
-  };
-
-  useEffect(() => {
-    getResponse(); 
-  }, []);
 
   return (
     <div>
-      <button onClick={getResponse}>Refresh Weather</button>
-      <h1>City: {response?.name}</h1>
-      <button onClick={changeCity}>Change City</button>
+      <form onSubmit={submitHandler}>
+        <input type="text" placeholder="Location" value={city} onChange={(e)=>{setCity(e.target.value);}}/>
+        <button onClick={getResponse}>Change</button>
+      </form>
+      <div className={styles.details}>
+        <div className="Photos">
+          <img src="https://i.pinimg.com/originals/90/85/bb/9085bb2580eee46db433164fdcca9500.gif" alt="" />
+        </div>
+        <div className={styles.weather}>
+          <h1>Location</h1>
+          <h4>Time</h4>
+          <h1>Clouds</h1>
+          <span>max:</span> 
+          <span>min:</span>
+        </div>
+      </div>
     </div>
   );
 };
