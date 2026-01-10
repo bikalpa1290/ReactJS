e084a823fb38d4a8a64fc71e0046ac2a90d91db3
bikalpa1290 <adhikaribikalpa1@gmail.com>
2026-01-10 09:49:25 +0545

    completed

--- Project6-todoList/src/assets/Components/Body.jsx
+++ Project6-todoList/src/assets/Components/Body.jsx
@@ -6,15 +6,25 @@ const Body = () => {
   const [form, setForm] = useState([]);
   const addForm=()=>{
     setForm(prev=>[...prev,prev.length])
+    console.log(form);
+  }
+  const deletForm=(id)=>{
+    let newform=form.filter(item=>item!==id)
+    setForm(newform)
+    console.log(form);
   }
   return (
     <div className={styles.main}>
         <h3>All task</h3>
         {form.map(id=>{
           return(
-          <Form key={id}/>)
+          <div>
+            <Form key={id} deletForm={deletForm} id={id}/>
+          </div>)
         })}
-        <Adder onAdd={addForm}/>
+        <div className={styles.add}>
+          <Adder onAdd={addForm}/>
+        </div>
     </div>
   )
 }
--- Project6-todoList/src/assets/Components/Form.jsx
+++ Project6-todoList/src/assets/Components/Form.jsx
@@ -2,14 +2,12 @@ import React, { useState } from 'react'
 import styles from "./styles.module.css"
 import 'remixicon/fonts/remixicon.css'
 
-const Form = () => {
+const Form = ({id,deletForm}) => {
   const [isComplete, setIsComplete] = useState(false);
   return (
     <div className={styles.formDiv}>
-      <i className={`ri-close-circle-fill ri-2x ${styles.icon1}`}></i>
-
+      <i className={`ri-close-circle-fill ri-2x ${styles.icon1}`} onClick={()=>{deletForm(id)}}></i>
       <input type="text" placeholder="Write task" className={isComplete==false?styles.inputIncomplete:styles.inputComplete} />
-
       <i className={`ri-checkbox-fill ri-2x ${styles.icon2}`}
       onClick={()=>
       {setIsComplete(prev=>!prev);}
